025685b1cbac8d8ddba61be9b41930908f20656d
Nathan Arthur <narthur-git@rainskit.com>
2013-08-22 00:24:32 -0400

    start working on queries to extract data from my textpattern instance

---
+++ query_textpattern.sql
@@ -0,0 +1,26 @@
+
+-- http://www.textpattern.net/wiki/index.php?title=Database_Schema_Reference
+
+-- these are all the articles and fields that matter to me:
+select
+	ID,
+	Posted, LastMod,
+	Title, url_title,
+	Image,
+	Section,
+	Keywords,
+	custom_2 as quote_attribution,
+	Body, Excerpt
+from txp_textpattern
+where
+	Status = 4
+	and Section <> 'Postmaster'
+order by ID
+;
+
+-- need to handle each section: blog, quotes, reference, (tag), preview
+
+-- need to get comments, files, forms, images, links?, pages
+
+-- need to browse through settings and plugins
+
